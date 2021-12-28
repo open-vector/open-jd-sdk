e74ba56dea9597909024b24a39817da82b90875d
NingGelin <455307376@qq.com>
2021-12-28 18:19:12 +0800

    接口调用优化

--- main.go
+++ main.go
@@ -8,13 +8,18 @@ import (
 )
 
 func main() {
-	fmt.Println("open-jd-sdk")
+	// 定义请求体
 	var goodReq model.JFGoodsReq
 	goodReq.EliteId = 2
 	jfQueryRequest := model.JFQueryRequest{GoodReq: goodReq}
 
+	// 实例化一个client
 	jdClient := client.New()
+
+	// 定义响应体
 	var jingfenQueryResult model.JingfenQueryResult
+	// 调用请求，并将结果反序列化到响应体里
+	// 这里发送请求已经抽象好，但是不知道是不是最好的方法，每个requet都要实现接口RequestInterface的GetByte方法，响应也没抽象
 	json.Unmarshal(jdClient.Execute("jd.union.open.goods.jingfen.query", jfQueryRequest), &jingfenQueryResult)
 	fmt.Println(jfQueryRequest)
 }
