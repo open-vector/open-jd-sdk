70dd9192c83a2e8af94659dfcaeea3ae75c151fc
NingGelin <455307376@qq.com>
2021-12-28 18:49:58 +0800

    接口调用优化

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ func main() {
 	var jingfenQueryResult model.JingfenQueryResult
 	// 调用请求，并将结果反序列化到响应体里
 	// 这里发送请求已经抽象好，但是不知道是不是最好的方法，每个requet都要实现接口RequestInterface的GetByte方法，响应也没抽象
-	// todo 待优化
+	// todo 待优化 这个method应该也是可以抽一下
 	json.Unmarshal(jdClient.Execute("jd.union.open.goods.jingfen.query", jfQueryRequest), &jingfenQueryResult)
 	fmt.Println(jfQueryRequest)
 }
